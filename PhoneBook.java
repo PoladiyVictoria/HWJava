3a8fc7e82c1169c708927ad60492823279b64077
PoladiyVictoria <vikapoladiy@gmail.com>
2023-09-27 22:39:12 +0300

    Не вводится имя

--- PhoneBook.java
+++ PhoneBook.java
@@ -2,18 +2,18 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class PhoneBook {
-    private static HashMap<Integer, String> phoneBook;
-    public PhoneBook(){
-        phoneBook = new HashMap<Integer, String>();
-    }
+    public static HashMap<Integer, String> phoneBook = new HashMap<Integer, String>();
+    // public PhoneBook(){
+    //     phoneBook = new HashMap<Integer, String>();
+    // }
     public static void writePhonebook(int col){
+        Scanner in = new Scanner(System.in);
         for (int i = 0; i < col; i++) {
-            Scanner in = new Scanner(System.in);
             System.out.println("Введите номер телефона!");
             Integer num = in.nextInt();
             System.out.println("Введите имя владельца телефона!");
             String name = in.nextLine();
-            phoneBook.put(num, name);
+            PhoneBook.phoneBook.put(num, name);
         }
     }
     public static void readPhonebook(String nam){
@@ -32,8 +32,8 @@ public class PhoneBook {
         Integer op = re.nextInt();
         if (op == 1) {
             System.out.println("Введите имя которое хотите найти!");
-            String name = re.nextLine();
-            PhoneBook.readPhonebook(name);
+            String fname = re.nextLine();
+            PhoneBook.readPhonebook(fname);
         }else if (op == 2) {
             System.out.println("Введите количество пользователей которых хотите добавить!");
             Integer current = re.nextInt();
